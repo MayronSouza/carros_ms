ea2a7129ab52c61d9fcd3d0c3c3ca6115ecdc4ad
MayronGMZ <mayron.souza@gmz.solutions>
2020-07-15 21:26:49 -0300

    Criando o pagina de login e decorando o TextFormField utilizando o InputDecoration

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        brightness: Brightness.light,
+        scaffoldBackgroundColor: Colors.white,
       ),
       home: LoginPage(),
     );
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -3,6 +3,81 @@ import 'package:flutter/material.dart';
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          'Carros',
+        ),
+      ),
+      body: _body(),
+    );
+  }
+
+  _body() {
+    return Container(
+      padding: EdgeInsets.all(16.0),
+      child: ListView(
+        children: <Widget>[
+          TextFormField(
+            style: TextStyle(
+              fontSize: 24.0,
+              color: Colors.blue,
+            ),
+            decoration: InputDecoration(
+              labelText: 'Login',
+              labelStyle: TextStyle(
+                color: Colors.grey,
+                fontSize: 24.0,
+              ),
+              hintText: 'Digite o login...',
+              hintStyle: TextStyle(
+                fontSize: 16.0,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 8.0,
+          ),
+          TextFormField(
+            obscureText: true,
+            style: TextStyle(
+              fontSize: 24.0,
+              color: Colors.blue,
+            ),
+            decoration: InputDecoration(
+              labelText: 'Senha',
+              labelStyle: TextStyle(
+                color: Colors.grey,
+                fontSize: 24.0,
+              ),
+              hintText: 'Digite a senha...',
+              hintStyle: TextStyle(
+                fontSize: 16.0,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 16.0,
+          ),
+          Container(
+            height: 48.0,
+            child: RaisedButton(
+              child: Text(
+                'Login',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24.0,
+                ),
+              ),
+              color: Colors.blue,
+              onPressed: () {
+                print('Você logou!!!');
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
