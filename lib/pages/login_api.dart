987525cfdae24bd6a5d02be0d0f26576362f6a43
MayronGMZ <mayron.souza@gmz.solutions>
2020-07-18 23:30:01 -0300

    Utilizando a url autenticada, que necessita dos headers com application/json

--- lib/pages/login_api.dart
+++ lib/pages/login_api.dart
@@ -1,17 +1,23 @@
+import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class LoginApi {
   static Future<bool> login(String login, String senha) async {
-    var url = 'http://livrowebservices.com.br/rest/login';
+    var url = 'https://carros-springboot.herokuapp.com/api/v2/login';
 
     Map params = {
-      'login': login,
-      'senha': senha,
+      'username': login,
+      'password': senha,
     };
+
+    Map<String, String> headers = {"Content-Type": "application/json"};
+
+    String s = json.encode(params);
 
     var response = await http.post(
       url,
-      body: params,
+      body: s,
+      headers: headers,
     );
 
     print('Response status: ${response.statusCode}');
